6fd50e430884a6e48c67886109ce673dbb2f32fa
demitycho <tanweihao94@gmail.com>
2018-04-19 16:46:44 +0800

    Update Credential layout

--- src/components/Credential.js
+++ src/components/Credential.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react'
+import { Image, Item } from 'semantic-ui-react'
 
 class Credential extends Component {
   render(){
     return(
-      <div>
-        {this.props.credential.jobName} - {this.props.credential.jobLocation}
-      </div>
+      <Item>
+        <Item.Content>
+          <Item.Header as='a'>{this.props.credential.jobName}</Item.Header>
+          <Item.Extra>{this.props.credential.jobLocation}</Item.Extra>
+          <Item.Meta>{this.props.credential.jobDescription}</Item.Meta>
+        </Item.Content>
+      </Item>
+
     )
   }
 }
--- src/components/Credentials.js
+++ src/components/Credentials.js
@@ -1,19 +1,21 @@
 import React, { Component } from 'react'
 import CredentialData from '../data/CredentialData.js'
 import Credential from './Credential.js'
-import { Header, Segment } from 'semantic-ui-react'
+import { Header, Item, Segment } from 'semantic-ui-react'
 
 class Credentials extends Component {
   render(){
     return(
       <Segment>
         <Header size='large'>Credentials</Header>
-        {CredentialData.map(credential => (
-          <Credential
-            key={credential.id}
-            credential={credential}
-          />
-        ))}
+        <Item.Group divided>
+          {CredentialData.map(credential => (
+            <Credential
+              key={credential.id}
+              credential={credential}
+            />
+          ))}
+        </Item.Group>
       </Segment>
     )
   }
